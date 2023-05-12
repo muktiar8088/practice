ac52b82b69a9f97688f0511f21c5e8815c4ec29a
muktiar <muktiar.snow@gmail.com>
2023-05-12 19:34:15 +0530

    inserting values into table

--- file1.sql
+++ file1.sql
@@ -1,5 +1,6 @@
 
+
 create database employee;
 create schema emp_schema;
 create table employees(eid number(10) primary key, ename varchar(20) ,salary number (10));
-
+insert into employees values(1,'muktiar',100000);
