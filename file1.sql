cf025aec5cc8ab7e756b69283282d81dddf7a6f3
muktiar <muktiar.snow@gmail.com>
2023-05-12 20:05:04 +0530

    inserting values into table and rename to emp

--- file1.sql
+++ file1.sql
@@ -1,6 +1,9 @@
 
 
+
 create database employee;
 create schema emp_schema;
 create table employees(eid number(10) primary key, ename varchar(20) ,salary number (10),department varchar(20));
 insert into employees values(1,'muktiar',100000);
+insert into employees values(2,'khadar',200000);
+alter table employees rename to emp;
